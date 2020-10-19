694d91b4db1413c90af6b8df5fcd177c625887fb
NMoore42 <nicholas.moore5@icloud.com>
2020-10-19 15:53:48 -0500

    basic login functionality complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+const loginEndpoint = "http://localhost:3000/login";
 
 
 class Login extends React.Component {
@@ -10,18 +11,45 @@ class Login extends React.Component {
     }
   }
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  loginObj = () => {
+    return {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    }
+  }
+
+  handleLoginSubmit = (event) => {
+    event.preventDefault()
+    fetch(loginEndpoint, this.loginObj())
+      .then(resp => resp.json())
+      .then(userData => console.log(userData))
+      .catch(err => new Error(err))
+  }
 
 
   render(){
+    const { username, password } = this.state
     return (
       <div>
-        <label htmlFor="username">Username: </label>
-        <input id="username" type="text"/>
-        <br/>
-        <label htmlFor="password">Password: </label>
-        <input id="password" type="password"/>
-        <br/>
-        <input type="submit" value="Login"/>
+        <form onSubmit={this.handleLoginSubmit}>
+          <label htmlFor="username">Username: </label>
+          <input onChange={this.handleChange} name="username" id="username" type="text" value={username}/>
+          <br/>
+          <label htmlFor="password">Password: </label>
+          <input onChange={this.handleChange} name="password" id="password" value={password} type="password"/>
+          <br/>
+          <input type="submit" value="Login"/>
+        </form>
       </div>
     )
   }
